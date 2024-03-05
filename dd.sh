29ff8042dba3827dbf6eccb4fb723f61ceb9b0f4
BKopyl <159447634+BKopyl@users.noreply.github.com>
2024-03-05 16:26:32 +0200

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-TOKEN=$(aws secretsmanager get-secret-value --secret-id github/token --query SecretString --output text)
-REPO_URL="https://raw.githubusercontent.com/BKopyl/ml-cluster/main/s3-run.sh"
-curl -H "Authorization: token ${TOKEN}" -o /home/ec2-user/dd.sh $REPO_URL
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:BKopyl/ml-cluster.git /home/ec2-user/
+
 bash /home/ec2-user/dd.sh
