f41a47a0fc1d8dee853804c8c4531cb9d6d25a32
BKopyl <159447634+BKopyl@users.noreply.github.com>
2024-03-05 18:51:03 +0200

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Пример аргументов
-# $1 - TARGET_DIR
-# $2 - REPO_URL
-# $3 - MAIN_BRANCH
-# $4 - SCRIPT_PATH
+# Установка безопасных директорий для Git
+git config --global --add safe.directory /home/ec2-user/ml-cluster
 
 TARGET_DIR="${1:-/home/ec2-user/ml-cluster}"
 REPO_URL="${2:-git@github.com:BKopyl/ml-cluster.git}"
 MAIN_BRANCH="${3:-main}"
 SCRIPT_PATH="${4:-/home/ec2-user/ml-cluster/s3-run.sh}"
 
+# Создание целевой директории
 mkdir -p "$TARGET_DIR"
 
 cd "$TARGET_DIR" || exit
 
+# Инициализация Git репозитория, если необходимо
 if [ ! -d ".git" ]; then
     git init
 fi
 
+# Настройка удаленного репозитория
 if git remote get-url origin &>/dev/null; then
     git remote set-url origin "$REPO_URL"
 else
@@ -27,8 +27,10 @@ fi
 
 export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 
+# Получение данных из репозитория
 git fetch
 
+# Переключение на основную ветку
 if git branch --list "$MAIN_BRANCH" &>/dev/null; then
     git checkout "$MAIN_BRANCH"
 else
@@ -39,7 +41,9 @@ git pull
 
 echo "Клонирование в $TARGET_DIR завершено."
 
+# Проверка и выполнение s3-run.sh
 if [ -f "$SCRIPT_PATH" ]; then
+    chmod +x "$SCRIPT_PATH"
     bash "$SCRIPT_PATH"
 else
     echo "Скрипт $SCRIPT_PATH не найден."
