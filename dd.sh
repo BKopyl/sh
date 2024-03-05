67a96f522a76b9affc998b23833ca18f971f8e4e
BKopyl <159447634+BKopyl@users.noreply.github.com>
2024-03-05 15:36:02 +0200

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TOKEN=$(aws secretsmanager get-secret-value --secret-id github/token --query SecretString --output text)
-REPO_URL="https://raw.githubusercontent.com/username/repository/branch/script.sh"
+REPO_URL="https://raw.githubusercontent.com/BKopyl/ml-cluster/main/s3-run.sh"
 curl -H "Authorization: token ${TOKEN}" -o /path/to/script.sh $REPO_URL
 bash /path/to/script.sh
