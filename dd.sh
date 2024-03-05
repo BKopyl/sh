8444f33a14729ba12b04e9c0044ef946f6b510da
BKopyl <159447634+BKopyl@users.noreply.github.com>
2024-03-05 23:31:10 +0200

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Принимаемые аргументы
 TARGET_DIR="$1"
 MAIN_BRANCH="$2"
 SCRIPT_NAME="$3"
 GITHUB_TOKEN="$4"
+SCRIPT_NAME="$5"
 
-# Путь к скрипту для скачивания
-SCRIPT_URL="https://raw.githubusercontent.com/BKopyl/ml-cluster/$MAIN_BRANCH/$SCRIPT_NAME"
-
-# Скачивание скрипта с использованием токена для аутентификации
-curl -H "Authorization: token $GITHUB_TOKEN" -L $SCRIPT_URL -o "$TARGET_DIR/$SCRIPT_NAME"
-
+wget --header="Authorization: token $GITHUB_TOKEN" -O "$TARGET_DIR/$SCRIPT_NAME" https://raw.githubusercontent.com/BKopyl/ml-cluster/main/s3-mount.sh
 if [ $? -eq 0 ]; then
-    echo "Скрипт $SCRIPT_NAME успешно скачан."
-    chmod +x "$TARGET_DIR/$SCRIPT_NAME"
-    # Выполнение скачанного скрипта
-    "$TARGET_DIR/$SCRIPT_NAME"
+    sudo chmod +x "$TARGET_DIR/$SCRIPT_NAME"
+    sudo bash "$TARGET_DIR/$SCRIPT_NAME"
 else
     echo "Ошибка при скачивании $SCRIPT_NAME с GitHub."
 fi
