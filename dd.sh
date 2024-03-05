0c928ea3316859508a88e0ba6f862243b664b24b
BKopyl <159447634+BKopyl@users.noreply.github.com>
2024-03-05 18:10:48 +0200

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-TARGET_DIR="/home/ec2-user/ml-cluster"
-REPO_URL="git@github.com:BKopyl/ml-cluster.git"
+# Пример аргументов
+# $1 - TARGET_DIR
+# $2 - REPO_URL
+# $3 - MAIN_BRANCH
+# $4 - SCRIPT_PATH
+
+TARGET_DIR="${1:-/home/ec2-user/ml-cluster}"
+REPO_URL="${2:-git@github.com:BKopyl/ml-cluster.git}"
+MAIN_BRANCH="${3:-main}"
+SCRIPT_PATH="${4:-/home/ec2-user/ml-cluster/s3-run.sh}"
 
 mkdir -p "$TARGET_DIR"
 
@@ -19,14 +27,6 @@ fi
 
 export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 
-if git ls-remote --heads "$REPO_URL" main &>/dev/null; then
-    MAIN_BRANCH="main"
-elif git ls-remote --heads "$REPO_URL" master &>/dev/null; then
-    MAIN_BRANCH="master"
-else
-    exit 1
-fi
-
 git fetch
 
 if git branch --list "$MAIN_BRANCH" &>/dev/null; then
@@ -39,7 +39,6 @@ git pull
 
 echo "Клонирование в $TARGET_DIR завершено."
 
-SCRIPT_PATH="/home/ec2-user/ml-cluster/s3-run.sh"
 if [ -f "$SCRIPT_PATH" ]; then
     bash "$SCRIPT_PATH"
 else
