4356e01edaf5fcd56afea9580024e8de54acb1e2
BKopyl <159447634+BKopyl@users.noreply.github.com>
2024-03-05 23:49:45 +0200

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -4,7 +4,6 @@ TARGET_DIR="$1"
 MAIN_BRANCH="$2"
 SCRIPT_NAME="$3"
 GITHUB_TOKEN="$4"
-SCRIPT_NAME="$5"
 
 wget --header="Authorization: token $GITHUB_TOKEN" -O "$TARGET_DIR/$SCRIPT_NAME" https://raw.githubusercontent.com/BKopyl/ml-cluster/main/s3-mount.sh
 if [ $? -eq 0 ]; then
